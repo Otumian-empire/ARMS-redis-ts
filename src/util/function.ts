c518b3d62e7670146f9a8c84eb91668dabe98c4e
otumian <popecan1000@gmail.com>
2022-08-23 08:46:07 +0000

    add util folder

---
+++ src/util/function.ts
@@ -0,0 +1,62 @@
+import { JWT_TTL, ONE, PAGINATION } from "./app.constant";
+
+export function generateToken() {
+  const SACK = "1234567890";
+  const SACK_LENGTH = SACK.length;
+  const TOKEN_LENGTH = 5;
+
+  let token = "";
+
+  for (let i = 0; i < TOKEN_LENGTH; i++) {
+    token += SACK[Math.floor(Math.random() * SACK_LENGTH)];
+  }
+
+  return token;
+}
+
+export function isValidRoomNumber(roomNumber = "") {
+  // A room number will be five characters starting with
+  // 'R' followed by the floor number and the room number
+  if (!roomNumber) return false;
+
+  const roomNumberLength = roomNumber.length;
+
+  if (roomNumberLength !== 5) return false;
+
+  if (!roomNumber.startsWith("R")) return false;
+
+  return !isNaN(Number(roomNumber.substring(1, roomNumberLength)));
+}
+
+export function isValidPrice(price = 0) {
+  return price && !isNaN(Number(price));
+}
+
+export function pagination(page = 1, pageSize = 12) {
+  if (page < ONE) {
+    page = PAGINATION.page;
+  }
+
+  if (pageSize < ONE) {
+    pageSize = PAGINATION.pageSize;
+  }
+
+  let offset = (page - ONE) * pageSize;
+  let limit = pageSize;
+
+  return { limit, skip: offset };
+}
+
+export function getJwtIat() {
+  return Date.now() + JWT_TTL;
+}
+
+export function getTokenFromHeader(req) {
+  const headerAuth = req.headers["authorization"];
+
+  if (!headerAuth) {
+    return "";
+  }
+
+  return headerAuth.split(" ")[1];
+}
