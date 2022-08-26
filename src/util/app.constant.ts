b0c64e776c2e4a6f97b8aadb2966de0efadc2103
otumian <popecan1000@gmail.com>
2022-08-26 05:59:03 +0000

    fix: Cannot find name 'process'

--- src/util/app.constant.ts
+++ src/util/app.constant.ts
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const rounds = parseInt(process.env.SALT_ROUNDS) || 10;
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const port = parseInt(process.env.PORT) || 3000;
-export const MINIMUM_PASSWORD_SIZE = 6;
-export const ONE = 1;
+export const rounds:number = parseInt(process.env.SALT_ROUNDS) || 10;
+export const JWT_SECRET:string = process.env.JWT_SECRET;
+export const port:number = parseInt(process.env.PORT) || 3000;
+export const MINIMUM_PASSWORD_SIZE:number = 6;
+export const ONE:number = 1;
 // set to one week if the JWT_TTL is not set. This is is microseconds
-export const JWT_TTL = parseInt(process.env.JWT_TTL) || 1000 * 60 * 60 * 24 * 7;
-export const PAGINATION = { page: 1, pageSize: 12 };
-export const REDIS_URI = process.env.REDIS_URI;
+export const JWT_TTL:number = parseInt(process.env.JWT_TTL) || 1000 * 60 * 60 * 24 * 7;
+export const PAGINATION:any = { page: 1, pageSize: 12 };
+export const REDIS_URI:string = process.env.REDIS_URI;
 // set to one hour if the REDIS_TTL is not set. This is in seconds
-export const REDIS_TTL = parseInt(process.env.REDIS_TTL) || 60 * 60;
+export const REDIS_TTL:number = parseInt(process.env.REDIS_TTL) || 60 * 60;
